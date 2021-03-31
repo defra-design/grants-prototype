c68004f90460f0c3178c91c601af89afdfabc02f
ellie <ellie.tms1371@gmail.com>
2021-03-31 14:57:17 +0100

    first-linting

--- public/javascripts/toc.js
+++ public/javascripts/toc.js
@@ -1,35 +1,30 @@
 
-
-  var ToC =
+var ToC =
     '<nav role="navigation" class="gem-c-contents-list gem-c-contents-list--no-underline">' +
       '<h2 class="gem-c-contents-list__title">Contents</h2>' +
-      '<ol class="gem-c-contents-list__list">';
+      '<ol class="gem-c-contents-list__list">'
 
-  var newLine, el, title, link;
+var newLine, el, title, link
 
-  $('h2').each(function() {
+$('h2').each(function () {
+  el = $(this)
+  title = el.text()
+  link = '#' + el.attr('id')
 
-    el = $(this);
-    title = el.text();
-    link = '#' + el.attr('id');
-
-    if ( link != '#undefined' ){
-
+  if (link != '#undefined') {
     newLine =
       '<li class="gem-c-contents-list__list-item gem-c-contents-list__list-item--dashed">' +
         '<a class="gem-c-contents-list__link " href="' + link + '">' +
           title +
         '</a>' +
-      '</li>';
+      '</li>'
 
-    ToC += newLine;
+    ToC += newLine
   }
+})
 
-  });
-
-  ToC +=
+ToC +=
      '</ol>' +
-    '</nav>';
+    '</nav>'
 
-
-  $('#toc').html(ToC);
+$('#toc').html(ToC)
