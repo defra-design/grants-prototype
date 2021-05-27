1ab0e0e4240b5a5200e3cd2ba0a0375489e07ec9
ogbk <ogbu.k.olu@gmail.com>
2021-05-27 13:16:50 +0100

    make gulpfile.js more legible

--- gulpfile.js
+++ gulpfile.js
@@ -16,28 +16,36 @@ requireDir('./gulp', { recurse: true })
 
 // gulp 4 requires dependency tasks to be defined before they are called.
 // We'll keep our top-level tasks in this file so that they are defined at the end of the chain, after their dependencies.
-gulp.task('generate-assets', gulp.series(
-  'clean',
-  'sass-extensions',
-  gulp.parallel(
-    'sass',
-    'copy-assets',
-    'sass-documentation',
-    'copy-assets-documentation',
-    'sass-v6',
-    'copy-assets-v6'
+gulp.task('generate-assets',
+  gulp.series(
+    'clean',
+    'sass-extensions',
+    gulp.parallel(
+      'sass',
+      'copy-assets',
+      'sass-documentation',
+      'copy-assets-documentation',
+      'sass-v6',
+      'copy-assets-v6'
+    )
   )
-))
-gulp.task('watch', gulp.parallel(
-  'watch-sass',
-  'watch-assets',
-  'watch-sass-v6',
-  'watch-assets-v6'
-))
-gulp.task('default', gulp.series(
-  'generate-assets',
+)
+
+gulp.task('watch',
   gulp.parallel(
-    'watch',
-    'server'
+    'watch-sass',
+    'watch-assets',
+    'watch-sass-v6',
+    'watch-assets-v6'
   )
-))
+)
+
+gulp.task('default',
+  gulp.series(
+    'generate-assets',
+    gulp.parallel(
+      'watch',
+      'server'
+    )
+  )
+)
