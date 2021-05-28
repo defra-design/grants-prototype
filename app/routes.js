9ea825a2c7a76e291f0053d1c98643520a4a38c8
ogbk <ogbu.k.olu@gmail.com>
2021-05-28 13:53:59 +0100

    cleanup console

--- app/routes.js
+++ app/routes.js
@@ -11,8 +11,8 @@ router.use('/', (req, res, next) => {
   req.subfolder = req.originalUrl.split('/')[2] // sub-folder e.g. 'service'
   res.locals.folder = req.folder // what folder the url is
   res.locals.subfolder = req.subfolder // what subfolder the URL is in
-  console.log('folder : ' + res.locals.folder + ', subfolder : ' + res.locals.subfolder)
-  console.log('previous page is: ' + res.locals.prevURL + ' and current page is ' + req.url + ' ' + res.locals.currentURL)
+  // console.log('folder : ' + res.locals.folder + ', subfolder : ' + res.locals.subfolder)
+  // console.log('previous page is: ' + res.locals.prevURL + ' and current page is ' + req.url + ' ' + res.locals.currentURL)
   console.log('\n-------------------------\n')
   next()
 })
