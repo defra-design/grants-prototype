f395b14d6f6ccc899da54c0249f64f812ae9fd7e
ogbk <ogbu.k.olu@gmail.com>
2021-05-31 17:51:31 +0100

    cleanup v2 routes

--- app/views/v2/routes/_routes.js
+++ app/views/v2/routes/_routes.js
@@ -8,8 +8,12 @@ const serviceName = 'Apply for a large countryside productivity grant for water'
 
 // Add your routes here - above the module.exports line
 
-//* ****************************************************
+// ****************************************************
+//* useful ??
+// DELETE SECTION ----
+
 // TASK LIST PAGE START //
+
 router.get('*XXX/task-list', function (req, res) {
   console.log('This is the task list')
 
@@ -44,7 +48,101 @@ router.get('*XXX/task-list', function (req, res) {
   })
 })
 
+// END DELETE SECTION ---
 // TASK LIST PAGE END //
+// *******************************//
+
+//* ****************************************************
+// TASK LIST PAGE START //
+router.get('/views/task-list', function (req, res) {
+  var application_status
+  var completed_sections
+
+  switch (req.session.data.completed_sections) {
+    case '0':
+      application_status = 'Expression of interest not started'
+      completed_sections = 'You have completed 0 of 4 sections.'
+      break
+    case '1':
+      application_status = 'Expression of interest in progress'
+      completed_sections = 'You have completed 1 of 4 sections.'
+      break
+    case '2':
+      application_status = 'Expression of interest in progress'
+      completed_sections = 'You have completed 2 of 4 sections.'
+      break
+    case '3':
+      application_status = 'Expression of interest in progress'
+      completed_sections = 'You have completed 3 of 4 sections.'
+      break
+    case '4':
+      application_status = 'Expression of interest completed'
+      completed_sections = 'You have completed 4 of 4 sections.'
+      break
+  }
+
+  project_eligibility_status = req.session.data.project_eligibility_status
+  project_eligibility_status_class = req.session.data.project_eligibility_status_class
+
+  theme_eligibility_status = req.session.data.theme_eligibility_status
+  theme_eligibility_status_class = req.session.data.theme_eligibility_status_class
+
+  contact_details_status = req.session.data.contact_details_status
+  contact_details_status_class = req.session.data.contact_details_status_class
+
+  project_benefits_status = req.session.data.project_benefits_status
+  project_benefits_status_class = req.session.data.project_benefits_status_class
+
+  final_project_details_status = req.session.data.final_project_details_status
+  final_project_details_status_class = req.session.data.final_project_details_status_class
+
+  res.render('./' + req.originalUrl, {
+
+    application_status: application_status,
+    completed_sections: completed_sections,
+    project_eligibility_status: project_eligibility_status,
+    project_eligibility_status_class: project_eligibility_status_class,
+    theme_eligibility_status: theme_eligibility_status,
+    theme_eligibility_status_class: theme_eligibility_status_class,
+    contact_details_status: contact_details_status,
+    contact_details_status_class: contact_details_status_class,
+    project_benefits_status: project_benefits_status,
+    project_benefits_status_class: project_benefits_status_class,
+    final_project_details_status: final_project_details_status,
+    final_project_details_status_class: final_project_details_status_class
+
+  })
+})
+
+// TASK LIST PAGE END //
+
+router.get('/views/start', function (req, res) {
+  // Set up all the section statuses
+
+  // Cannot start yet = 'govuk-tag--grey'
+  // Not started = 'govuk-tag--grey'
+  // In progress = 'govuk-tag--blue'
+  // Completed = ''
+
+  req.session.data.completed_sections = '0'
+
+  req.session.data.project_eligibility_status = 'Not started'
+  req.session.data.project_eligibility_status_class = ''
+
+  req.session.data.theme_eligibility_status = 'Cannot start yet'
+  req.session.data.theme_eligibility_status_class = 'govuk-tag--grey'
+
+  req.session.data.contact_details_status = 'Cannot start yet'
+  req.session.data.contact_details_status_class = 'govuk-tag--grey'
+
+  req.session.data.project_benefits_status = 'Cannot start yet'
+  req.session.data.project_benefits_status_class = 'govuk-tag--grey'
+
+  req.session.data.final_project_details_status = 'Cannot start yet'
+  req.session.data.final_project_details_status_class = 'govuk-tag--grey'
+
+  res.render('./' + req.originalUrl)
+})
 
 router.get('*/farming-type', function (req, res) {
   // test to check this section isn't completed...
