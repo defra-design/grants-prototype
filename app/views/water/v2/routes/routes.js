0904fcbad3b3bc3d378f4d1fee8c4e845d21ae59
ogbk <ogbu.k.olu@gmail.com>
2021-06-02 19:58:13 +0100

    delete unused route in /v2

--- app/views/water/v2/routes/routes.js
+++ app/views/water/v2/routes/routes.js
@@ -8,50 +8,6 @@ const serviceName = 'Apply for a large countryside productivity grant for water'
 
 // Add your routes here - above the module.exports line
 
-// ****************************************************
-//* useful ??
-// DELETE SECTION ----
-
-// TASK LIST PAGE START //
-
-router.get('*XXX/task-list', function (req, res) {
-  console.log('This is the task list')
-
-  // Cannot start yet = 'govuk-tag--grey'
-  // Not started = 'govuk-tag--grey'
-  // In progress = 'govuk-tag--blue'
-  // Completed = ''
-
-  // part0101status = req.session.data['part0101status']
-  part0101status = 'Completed'
-  part0101statusClass = ''
-
-  part0102status = 'In progress'
-  part0102statusClass = 'govuk-tag--blue'
-
-  part0201status = 'Not started'
-  part0201statusClass = 'govuk-tag--grey'
-
-  part0202status = 'Cannot start yet'
-  part0202statusClass = 'govuk-tag--grey'
-
-  res.render('./' + req.originalUrl, {
-    serviceName: serviceName,
-    part0101status: part0101status,
-    part0101statusClass: part0101statusClass,
-    part0102status: part0102status,
-    part0102statusClass: part0102statusClass,
-    part0201status: part0201status,
-    part0201statusClass: part0201statusClass,
-    part0202status: part0202status,
-    part0202statusClass: part0202statusClass
-  })
-})
-
-// END DELETE SECTION ---
-// TASK LIST PAGE END //
-// *******************************//
-
 //* ****************************************************
 // TASK LIST PAGE START //
 router.get('/views/task-list', function (req, res) {
