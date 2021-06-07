716e4648fed2d9ddf3c6f5cf321c70ac252116de
ogbk <ogbu.k.olu@gmail.com>
2021-06-07 11:24:01 +0100

    cleanup unused code

--- app/views/water/current/routes/routes.js
+++ app/views/water/current/routes/routes.js
@@ -3,165 +3,17 @@ const router = express.Router()
 
 const serviceName = 'Apply for a large grant for a water resource management project'
 
-// console.log( 'This is the _routes file' );
 console.log('Service name: ' + serviceName)
 
-// Add your routes here - above the module.exports line
-
-//* ****************************************************
-// WATER START PAGE //
-router.get('*/start', function (req, res) {
-  // console.log( 'This is the start page' );
-
-  // Cannot start yet = 'govuk-tag--grey'
-  // Not started = 'govuk-tag--grey'
-  // In progress = 'govuk-tag--blue'
-  // Completed = ''
-
-  // We only count the sections (weird part of the pattern tbh nicprice comment)
-
-  // Part 1: Check if you can apply
-
-  // Section 1: Check now
-  req.session.data.water_s01_status = 'Not started'
-  req.session.data.water_s01_status_class = 'govuk-tag--grey'
-
-  // Part 2: Express interest
-
-  // Section 2: Provide project details and benefits
-  req.session.data.water_s02_status = 'Cannot start yet'
-  req.session.data.water_s02_status_class = 'govuk-tag--grey'
-
-  // Section 3 = 'Give contact details'
-  req.session.data.water_s03_status = 'Cannot start yet'
-  req.session.data.water_s03_status_class = 'govuk-tag--grey'
-
-  // Section 4 = 'Full application'
-  req.session.data.water_s04_status = 'Cannot start yet'
-  req.session.data.water_s04_status_class = 'govuk-tag--grey'
-
+router.get('/start', function (req, res) {
   // ------------------------------------------
   req.session.data.COMPLETED = false
   // ------------------------------------------
 
-  res.render('./' + req.originalUrl, {
-  })
+  res.render('./' + req.originalUrl, {})
 })
 
-// START PAGE END //
-
-router.get('*/task-list-prefilled', function (req, res) {
-  req.session.data.water_s01_status = 'Completed'
-  req.session.data.water__s01_status_class = ''
-
-  req.session.data.water_s02_status = 'Not started'
-  req.session.data.water_s02_status_class = 'govuk-tag--grey'
-
-  req.session.data.water_s03_status = 'Cannot start yet'
-  req.session.data.water_s03_status_class = 'govuk-tag--grey'
-
-  req.session.data.water_s04_status = 'Cannot start yet'
-  req.session.data.water_s04_status_class = 'govuk-tag--grey'
-
-  res.redirect('task-list')
-})
-
-//* ****************************************************
-// TASK LIST PAGE START //
-router.get('*/task-list', function (req, res) {
-  // console.log( 'This is the task list' );
-
-  // Cannot start yet = 'govuk-tag--grey'
-  // Not started = 'govuk-tag--grey'
-  // In progress = 'govuk-tag--blue'
-  // Completed = ''
-
-  var backUrl = res.locals.prevURL
-
-  if (req.session.data.water_s01_status != 'Completed') {
-    // Part 1: Check if you can apply
-    // Section 1: Check now
-    req.session.data.water_s01_status = 'Not started'
-    req.session.data.water_s01_status_class = 'govuk-tag--grey'
-
-    // Part 2: Express interest
-    // Section 2: Provide project details and benefits
-    req.session.data.water_s02_status = 'Cannot start yet'
-    req.session.data.water_s02_status_class = 'govuk-tag--grey'
-
-    // Section 3 = 'Give contact details'
-    req.session.data.water_s03_status = 'Cannot start yet'
-    req.session.data.water_s03_status_class = 'govuk-tag--grey'
-
-    // Part 3: Apply in full
-    // Section 4: Complete the full application
-    req.session.data.water_s04_status = 'Cannot start yet'
-    req.session.data.water_s04_status_class = 'govuk-tag--grey'
-  }
-
-  var water_application_status
-  var water_completed_sections
-
-  switch (req.session.data.water_completed_sections) {
-    case '0':
-      water_application_status = 'Expression of interest not started'
-      water_completed_sections = 'You have completed 0 of 3 sections.'
-      break
-    case '1':
-      water_application_status = 'Expression of interest in progress'
-      water_completed_sections = 'You have completed 1 of 3 sections.'
-      break
-    case '2':
-      water_application_status = 'Expression of interest in progress'
-      water_completed_sections = 'You have completed 2 of 3 sections.'
-      break
-    case '3':
-      water_application_status = 'Expression of interest in progress'
-      water_completed_sections = 'You have completed 3 of 3 sections.'
-      break
-  }
-
-  var water_s01_status = req.session.data.water_s01_status
-  var water_s01_status_class = req.session.data.water_s01_status_class
-
-  // Part 2: Express interest
-
-  // Section 2: Provide project details and benefits
-  var water_s02_status = req.session.data.water_s02_status
-  var water_s02_status_class = req.session.data.water_s02_status_class
-
-  // Section 3 = 'Give contact details'
-  var water_s03_status = req.session.data.water_s03_status
-  var water_s03_status_class = req.session.data.water_s03_status_class
-
-  // Part 3: Apply in full
-
-  // Section 4: Complete the full application
-  var water_s04_status = req.session.data.water_s04_status
-  var water_s04_status_class = req.session.data.water_s04_status_class
-
-  res.render('./' + req.originalUrl, {
-    backUrl: backUrl,
-    water_application_status: water_application_status,
-    water_completed_sections: water_completed_sections,
-    water_s01_status: water_s01_status,
-    water_s01_status_class: water_s01_status_class,
-    water_s02_status: water_s02_status,
-    water_s02_status_class: water_s02_status_class,
-    water_s03_status: water_s03_status,
-    water_s03_status_class: water_s03_status_class,
-    water_s04_status: water_s04_status,
-    water_s04_status_class: water_s04_status_class
-  })
-})
-
-// TASK LIST PAGE END //
-
-// SECTION 1
-
-// Q: FARMING TYPE
-
-router.get('*/farming-type', function (req, res) {
+router.get('/farming-type', function (req, res) {
   var backUrl = 'start'
   var nextUrl = 'farming-type-answer'
   var completedUrl = 'farming-type-answer-completed'
