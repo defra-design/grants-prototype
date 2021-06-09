7f24e38b5557465241ee1082a83262e632915b5b
ogbk <ogbu.k.olu@gmail.com>
2021-06-09 11:56:26 +0100

    change 117

--- app/utils.js
+++ app/utils.js
@@ -7,7 +7,7 @@ const serviceName = {
     v5: 'Apply for a Farming Transformation Fund water resource management grant',
     v6: 'Check if you can apply for a water resource management grant',
     v7private: 'Check if you can apply for a water resource management grant',
-    current: 'Check if you can apply for a Farming Transformation Fund water grant'
+    current: 'Check if you can apply for a Farming Transformation Fund water management grant'
   },
   slurry: {
     v3: 'Apply for a large grant for slurry equipment',
