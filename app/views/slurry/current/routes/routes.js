7e42302953b0aa5d53609a44d7c6418a2009950d
Robert Nagy <robert.a.nagy@capgemini.com>
2021-06-25 12:15:09 +0100

    slurry v1 baseline

---
+++ app/views/slurry/current/routes/routes.js
@@ -0,0 +1,563 @@
+const express = require('express')
+const router = express.Router()
+
+const serviceName = 'Check if you can apply for a Farming Transformation Fund slurry acidification grant'
+
+console.log('Service name: ' + serviceName)
+
+router.get('/start', function (req, res) {
+  // =================================
+  req.session.data = {
+    COMPLETED: false,
+    DETAILS: false
+  }
+  // =================================
+
+  var nextUrl = 'farming-type'
+
+  res.render('./' + req.originalUrl, {
+    nextUrl
+  })
+})
+
+router.get('/farming-type', function (req, res) {
+  var backUrl = 'start'
+  var nextUrl = 'farming-type-answer'
+  var completedUrl = 'farming-type-answer-completed'
+
+  res.render('./' + req.originalUrl, {
+    backUrl,
+    nextUrl,
+    completedUrl
+  })
+})
+
+router.get('/farming-type-answer', function (req, res) {
+  var farmingType = req.session.data['farming-type']
+  var farmingTypeOther = req.session.data['farming-type-other-options']
+
+  if (!!farmingType && farmingType === 'Something else') {
+    res.redirect('project-about-fail')
+  }
+  if (!!farmingType && farmingType === 'no' && !!farmingTypeOther) {
+    if (farmingTypeOther === 'something else') {
+      res.redirect('project-about-fail')
+    } else {
+      farmingType = 'no: [' + farmingTypeOther + ']'
+    }
+  }
+
+  req.session.data['summary-farming-type'] = farmingType
+  res.redirect('legal-status')
+})
+
+router.get('/farming-type-answer-completed', function (req, res) {
+  var farmingType = req.session.data['farming-type']
+  var farmingTypeOther = req.session.data['farming-type-other-options']
+
+  if (!!farmingType && farmingType === 'Something else') {
+    res.redirect('project-about-fail')
+  }
+
+  if (!!farmingType && farmingType === 'no' && !!farmingTypeOther) {
+    if (farmingTypeOther === 'something else') {
+      res.redirect('project-about-fail')
+    } else {
+      farmingType = 'no: [' + farmingTypeOther + ']'
+    }
+  }
+
+  req.session.data['summary-farming-type'] = farmingType
+  res.redirect('answers')
+})
+
+// Q: LEGAL STATUS
+
+router.get('/legal-status', function (req, res) {
+  var backUrl = 'farming-type'
+  var nextUrl = 'legal-status-answer'
+  var completedUrl = 'legal-status-answer-completed'
+
+  res.render('./' + req.originalUrl, {
+    backUrl,
+    nextUrl,
+    completedUrl
+  })
+})
+
+router.post('/legal-status-answer', function (req, res) {
+  var legalStatus = req.session.data['legal-status']
+
+  if (legalStatus === 'None') { res.redirect('legal-status-fail') } else { res.redirect('country') }
+})
+
+router.post('/legal-status-answer-completed', function (req, res) {
+  var legalStatus = req.session.data['legal-status']
+
+  if (legalStatus === 'None') { res.redirect('legal-status-fail') } else { res.redirect('answers') }
+})
+
+// Q : Country
+
+router.get('/country', function (req, res) {
+  var backUrl = 'legal-status'
+  var nextUrl = 'country-answer'
+  var completedUrl = 'country-answer-completed'
+
+  res.render('./' + req.originalUrl, {
+    backUrl,
+    nextUrl,
+    completedUrl
+  })
+})
+
+router.post('/country-answer', function (req, res) {
+  var country = req.session.data.country
+  var postcode = req.session.data.postcode
+  if (!!country && country === 'yes') {
+    if (!!postcode && postcode.length > 0) {
+      country = 'yes: [ Postcode: ' + postcode + ' ]'
+    }
+    req.session.data['summary-country'] = country
+    res.redirect('planning-permission')
+  } else {
+    res.redirect('country-fail')
+  }
+})
+
+router.post('/country-answer-completed', function (req, res) {
+  var country = req.session.data.country
+  var postcode = req.session.data.postcode
+
+  if (!!country && country === 'yes') {
+    if (!!postcode && postcode.length > 0) {
+      country = 'yes: [ Postcode: ' + postcode + ' ]'
+    }
+
+    req.session.data['summary-country'] = country
+    res.redirect('answers')
+  } else {
+    res.redirect('country-fail')
+  }
+})
+
+// PROJECT START
+
+router.get('/project-start', function (req, res) {
+  var planningPermission = req.session.data['planning-permission']
+
+  var backUrl
+  var nextUrl = 'project-start-answer'
+  var completedUrl = 'project-start-answer-completed'
+
+  if (planningPermission === 'Not needed' || planningPermission === 'Secured') {
+    backUrl = 'planning-permission'
+  } else if (planningPermission === 'maybe') {
+    backUrl = 'planning-required-condition'
+  } else {
+    backUrl = 'planning-permission-fail'
+  }
+
+  res.render('./' + req.originalUrl, {
+    backUrl,
+    nextUrl,
+    completedUrl
+  })
+})
+
+router.post('/project-start-answer', function (req, res) {
+  var projectStart = req.session.data['project-start']
+
+  if (projectStart === 'project work') { res.redirect('project-start-fail') } else { res.redirect('tenancy') }
+})
+
+router.post('/project-start-answer-completed', function (req, res) {
+  var projectStart = req.session.data['project-start']
+
+  if (projectStart === 'project work') { res.redirect('project-start-fail') } else { res.redirect('answers') }
+})
+
+// Q: Tenancy
+
+router.get('/tenancy', function (req, res) {
+  var backUrl = 'project-start'
+  var nextUrl = 'tenancy-answer'
+  var completedUrl = 'answers'
+
+  res.render('./' + req.originalUrl, {
+    backUrl,
+    nextUrl,
+    completedUrl
+  })
+})
+
+router.post('/tenancy-answer', function (req, res) {
+  var tenant = req.session.data.tenancy
+
+  if (tenant === 'Yes') {
+    res.redirect('project-items')
+  } else { res.redirect('tenancy-length') }
+})
+
+router.post('/tenancy-length-answer', function (req, res) {
+  var tenancyLength = req.session.data['tenancy-length']
+
+  if (tenancyLength === 'No') { res.redirect('tenancy-length-condition') } else { res.redirect('project-items') }
+})
+
+router.post('/tenancy-length-answer-completed', function (req, res) {
+  res.redirect('answers')
+})
+
+
+// Q: Project items
+
+router.get('/project-items', function (req, res) {
+  var backUrl = 'tenancy-length'
+  var nextUrl = 'project-items-answers'
+  var completedUrl = 'answers'
+
+  res.render('./' + req.originalUrl, {
+    backUrl,
+    nextUrl,
+    completedUrl
+  })
+})
+
+
+router.post('/project-items-answers', function (req, res) {
+  var projectItemsAnswers = req.session.data['project-items']
+
+  if (projectItemsAnswers == 'Mild acidification equipment' || projectItemsAnswers == 'Plasma')   { res.redirect('associated-works') } else { res.redirect('treatment-fail') }
+})
+
+
+// Q: Associated works
+
+router.get('/associated-works', function (req, res) {
+  var backUrl = 'project-items'
+  var nextUrl = 'project-cost'
+  var completedUrl = 'answers'
+
+  res.render('./' + req.originalUrl, {
+    backUrl,
+    nextUrl,
+    completedUrl
+  })
+})
+
+// Q: Project cost
+
+router.get('/project-cost', function (req, res) {
+  req.session.data.currentProjectCost = req.session.data['project-cost']
+
+  var backUrl = 'associated-works'
+  var nextUrl = 'project-cost-answer'
+  var completedUrl = 'project-cost-answer-completed'
+
+  res.render('./' + req.originalUrl, {
+    backUrl,
+    nextUrl,
+    completedUrl
+  })
+})
+
+router.post('/project-cost-answer', function (req, res) {
+  var projectCost = req.session.data['project-cost']
+
+  if (projectCost < 87500) { res.redirect('project-cost-fail') } else { res.redirect('grant') }
+})
+
+router.post('/project-cost-answer-completed', function (req, res) {
+  var projectCost = req.session.data['project-cost']
+
+  if (projectCost < 87500) { res.redirect('project-cost-fail') } else { res.redirect('answers') }
+})
+
+router.get('/grant', function (req, res) {
+  var backUrl = 'project-cost'
+  var nextUrl = 'remaining-costs'
+  var completedUrl = 'answers'
+
+  res.render('./' + req.originalUrl, {
+    backUrl,
+    nextUrl,
+    completedUrl
+  })
+})
+
+// Q: remaining costs
+
+router.get('/remaining-costs', function (req, res) {
+  var backUrl = 'grant'
+  var nextUrl = 'remaining-costs-answer'
+  var completedUrl = 'remaining-costs-answer-completed'
+
+  res.render('./' + req.originalUrl, {
+    backUrl,
+    nextUrl,
+    completedUrl
+  })
+})
+
+router.post('/remaining-costs-answer', function (req, res) {
+  var remainingCosts = req.session.data['remaining-costs']
+
+  if (remainingCosts === 'no') { res.redirect('remaining-costs-fail') } else { res.redirect('water-SSSI') }
+})
+
+router.post('/remaining-costs-answer-completed', function (req, res) {
+  var remainingCosts = req.session.data['remaining-costs']
+
+  if (remainingCosts === 'no') { res.redirect('remaining-costs-fail') } else { res.redirect('answers') }
+})
+
+// PLANNING PERMISSION
+
+router.get('/planning-permission', function (req, res) {
+  // var planningPermission = req.session.data['planning-permission']
+  var backUrl = 'country'
+  var nextUrl = 'planning-permission-answer'
+  var completedUrl = 'answers'
+
+  res.render('./' + req.originalUrl, {
+    backUrl,
+    nextUrl,
+    completedUrl
+  })
+})
+
+// PLANNING PERMISSION COMPLETED
+router.post('/planning-permission-answer', function (req, res) {
+  var planningPermission = req.session.data['planning-permission']
+
+  if (planningPermission === 'Not needed' || planningPermission === 'Secured') {
+    res.redirect('project-start')
+  }
+
+  if (planningPermission === 'maybe') {
+    res.redirect('planning-required-condition')
+  }
+
+  res.redirect('planning-permission-fail')
+})
+
+// PLANNING PERMISSION CONDITION
+router.get('/planning-required-condition', function (req, res) {
+  var backUrl = 'planning-permission'
+  var nextUrl = 'project-start'
+
+  res.render('./' + req.originalUrl, {
+    backUrl,
+    nextUrl
+  })
+})
+
+// Water SSSI
+
+router.get('/water-SSSI', function (req, res) {
+  var nextUrl = 'projects-impact'
+  var backUrl = 'remaining-costs'
+  var completedUrl = 'answers'
+
+  res.render('./' + req.originalUrl, {
+    backUrl,
+    nextUrl,
+    completedUrl
+  })
+})
+
+// PROJECTS IMPACT (SLURRY)
+router.get('/projects-impact', function (req, res) {
+  var backUrl = 'water-SSSI'
+  var nextUrl = 'slurry-currently-treated'
+  var completedUrl = 'answers'
+
+  res.render('./' + req.originalUrl, {
+    backUrl,
+    nextUrl,
+    completedUrl
+  })
+})
+
+
+// SLURRY CURRENTLY TREATED (SLURRY)
+router.get('/slurry-currently-treated', function (req, res) {
+  var backUrl = 'projects-impact'
+  var nextUrl = 'slurry-to-be-treated'
+  var completedUrl = 'answers'
+
+  res.render('./' + req.originalUrl, {
+    backUrl,
+    nextUrl,
+    completedUrl
+  })
+})
+
+// SLURRY TO-BE TREATED (SLURRY)
+router.get('/slurry-to-be-treated', function (req, res) {
+  var backUrl = 'slurry-currently-treated'
+  var nextUrl = 'answers'
+  var completedUrl = 'answers'
+
+  res.render('./' + req.originalUrl, {
+    backUrl,
+    nextUrl,
+    completedUrl
+  })
+})
+
+
+
+// answers
+
+router.get('/answers', function (req, res) {
+  req.session.data.COMPLETED = true
+
+  var backUrl = 'answers-back'
+  var nextUrl = 'business'
+
+  res.render('./' + req.originalUrl, {
+    backUrl,
+    nextUrl
+  })
+})
+
+router.get('/answers-back', function (req, res) {
+  req.session.data.COMPLETED = false
+  res.redirect('slurry-to-be-treated')
+})
+
+// next-steps 'removed from current design/thinking'
+
+router.get('/next-steps', function (req, res) {
+  var backUrl = 'answers'
+  var nextUrl = 'business'
+
+  res.render('./' + req.originalUrl, {
+    backUrl,
+    nextUrl
+  })
+})
+
+// business
+
+router.get('/business', function (req, res) {
+  var backUrl = 'answers'
+  var nextUrl = 'applying'
+  var detailsUrl = 'check-details'
+
+  res.render('./' + req.originalUrl, {
+    nextUrl,
+    backUrl,
+    detailsUrl
+  })
+})
+
+// applying
+router.get('/applying', function (req, res) {
+  var backUrl = 'business'
+  var nextUrl = 'applying-answer'
+  var detailsUrl = 'check-details'
+
+  res.render('./' + req.originalUrl, {
+    nextUrl,
+    backUrl,
+    detailsUrl
+  })
+})
+
+router.post('/applying-answer', function (req, res) {
+  const { applicant } = req.session.data
+
+  if (applicant === 'Agent') {
+    res.redirect('agent-details')
+  }
+
+  res.redirect('farmer-details')
+})
+
+router.get('/agent-details', function (req, res) {
+  var backUrl = 'applying'
+  var nextUrl = 'farmer-details'
+  var detailsUrl = 'check-details'
+
+  res.render('./' + req.originalUrl, {
+    backUrl,
+    nextUrl,
+    detailsUrl
+  })
+})
+
+router.get('/farmer-details', function (req, res) {
+  var backUrl = req.session.data.applicant === 'Agent' ? 'agent-details' : 'applying'
+  var nextUrl = 'check-details'
+  var detailsUrl = 'check-details'
+
+  res.render('./' + req.originalUrl, {
+    backUrl,
+    nextUrl,
+    detailsUrl
+  })
+})
+
+// check-details
+
+router.get('/check-details', function (req, res) {
+  req.session.data.DETAILS = true
+
+  var backUrl = 'check-details-back'
+  var nextUrl = 'consent'
+
+  res.render('./' + req.originalUrl, {
+    backUrl,
+    nextUrl
+  })
+})
+
+router.get('/check-details-back', function (req, res) {
+  req.session.data.DETAILS = false
+  res.redirect('farmer-details')
+})
+
+router.get('/agent-farmer-details', function (req, res) {
+  var nextUrl = 'check-details'
+  var backUrl = 'your-details'
+
+  res.render('./' + req.originalUrl, {
+    backUrl,
+    nextUrl
+  })
+})
+
+// consent
+
+router.get('/consent', function (req, res) {
+  var nextUrl = 'reference-number'
+  var backUrl = 'check-details'
+
+  res.render('./' + req.originalUrl, {
+    backUrl,
+    nextUrl
+  })
+})
+
+// reference-number
+
+router.get('/reference-number', function (req, res) {
+  var backUrl = 'consent'
+
+  res.render('./' + req.originalUrl, {
+    backUrl
+  })
+})
+
+router.get('/survey', function (req, res) {
+  res.render('./' + req.originalUrl, {})
+})
+
+router.get('/email', function (req, res) {
+  res.render('./' + req.originalUrl, {})
+})
+
+module.exports = router
