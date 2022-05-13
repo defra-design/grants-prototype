383e4467eeba580919fea78b01cf0b58e86942f1
ogbk <ogbu.k.olu@gmail.com>
2022-05-13 18:12:26 +0100

    update values

--- app/views/slurrystorage/utils.js
+++ app/views/slurrystorage/utils.js
@@ -1,10 +1,10 @@
 const projectItemsCost = {
   agitator: { text: 'Agitator', value: 1000 },
-  underFloorStorage: { text: 'Under-floor storage and transfer channels', value: 2000 },
+  underFloorStorage: { text: 'Under-floor storage and transfer channels', value: 2100 },
   receptionTanksPits: { text: 'Reception tanks/pits', value: 3250 },
-  laddersPlatforms: { text: 'Ladders and platforms', value: 4250 },
-  agitatorPlatform: { text: 'Agitator platform', value: 52500 },
-  healthSafetyFeatures: { text: 'Health and safety features', value: 6250 }
+  laddersPlatforms: { text: 'Ladders and platforms', value: 4550 },
+  agitatorPlatform: { text: 'Agitator platform', value: 3190 },
+  healthSafetyFeatures: { text: 'Health and safety features', value: 2440 }
 }
 
 module.exports = { projectItemsCost }
