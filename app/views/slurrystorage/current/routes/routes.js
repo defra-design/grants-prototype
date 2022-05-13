fec8c3d5fb788aea424b107d99018264bac42f13
ogbk <ogbu.k.olu@gmail.com>
2022-05-13 17:58:57 +0100

    export project items data

--- app/views/slurrystorage/current/routes/routes.js
+++ app/views/slurrystorage/current/routes/routes.js
@@ -3,14 +3,7 @@ const router = express.Router()
 
 const serviceName = 'Check if you can apply for a Farming Transformation Fund slurry acidification grant'
 
-const projectItemsCost = {
-  agitator: { text: 'Agitator', value: 1000 },
-  underFloorStorage: { text: 'Under-floor storage and transfer channels', value: 2000 },
-  receptionTanksPits: { text: 'Reception tanks/pits', value: 3250 },
-  laddersPlatforms: { text: 'Ladders and platforms', value: 4250 },
-  agitatorPlatform: { text: 'Agitator platform', value: 52500 },
-  healthSafetyFeatures: { text: 'Health and safety features', value: 6250 }
-}
+const { projectItemsCost } = require('../../utils')
 
 console.log('Service name: ' + serviceName)
 
---
+++ app/views/slurrystorage/utils.js
@@ -0,0 +1,10 @@
+const projectItemsCost = {
+  agitator: { text: 'Agitator', value: 1000 },
+  underFloorStorage: { text: 'Under-floor storage and transfer channels', value: 2000 },
+  receptionTanksPits: { text: 'Reception tanks/pits', value: 3250 },
+  laddersPlatforms: { text: 'Ladders and platforms', value: 4250 },
+  agitatorPlatform: { text: 'Agitator platform', value: 52500 },
+  healthSafetyFeatures: { text: 'Health and safety features', value: 6250 }
+}
+
+module.exports = { projectItemsCost }
