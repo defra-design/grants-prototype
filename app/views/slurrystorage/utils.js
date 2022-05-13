391ea25546b7152b97a1fcefccaaebd29385b5bf
ogbk <ogbu.k.olu@gmail.com>
2022-05-14 00:11:41 +0100

    complete utils file

--- app/views/slurrystorage/utils.js
+++ app/views/slurrystorage/utils.js
@@ -1,10 +1,10 @@
 const projectItemsCost = {
-  agitator: { text: 'Agitator', value: 1000 },
-  underFloorStorage: { text: 'Under-floor storage and transfer channels', value: 2100 },
-  receptionTanksPits: { text: 'Reception tanks/pits', value: 3250 },
-  laddersPlatforms: { text: 'Ladders and platforms', value: 4550 },
-  agitatorPlatform: { text: 'Agitator platform', value: 3190 },
-  healthSafetyFeatures: { text: 'Health and safety features', value: 2440 }
+  agitator: { text: 'Agitator', value: 1000, hint: 'Item cost: £1,000' },
+  underFloorStorage: { text: 'Under-floor storage and transfer channels', value: 2100, hint: 'Item cost: £2,100' },
+  receptionTanksPits: { text: 'Reception tanks/pits', value: 3250, hint: 'Item cost: £3,250' },
+  laddersPlatforms: { text: 'Ladders and platforms', value: 4550, hint: 'Item cost: £4,550' },
+  agitatorPlatform: { text: 'Agitator platform', value: 3190, hint: 'Item cost: £3,190' },
+  healthSafetyFeatures: { text: 'Health and safety features', value: 2440, hint: 'For example, gas monitor, tyre ladders<br/>Item cost: £2,440' }
 }
 
 module.exports = { projectItemsCost }
