39d785173f5169fd9a7bcbfaf966a51c52573914
Robert Nagy <robert.a.nagy@capgemini.com>
2022-05-16 13:01:50 +0100

    standard cost updates v1

--- app/views/slurrystorage/utils.js
+++ app/views/slurrystorage/utils.js
@@ -1,17 +1,19 @@
 const storeTypeCost = {
-  earthBankedLagoon: { text: 'Earth-banked lagoon', value: 9000, formattedValue: '£9,000', hint: 'Item cost: £9,000' },
-  linedLagoon: { text: 'Lined lagoon', value: 2100, formattedValue: '£2,100', hint: 'Item cost: £2,100' },
-  aboveGroundCyclindralTank: { text: 'Above-ground cyclindral tank', value: 20250, formattedValue: '£20,250', hint: 'Item cost: £20,250' },
-  insituConcreteStore: { text: 'In situ concrete store', value: 10000, formattedValue: '£10,000', hint: 'Item cost: £10,000' },
-  precastConcreteStore: { text: 'Precast concrete store', value: 8000, formattedValue: '£8,000', hint: 'Item cost: £8,000' },
-  undefloorSlattedStore: { text: 'Underfloor slatted store', value: 21500, formattedValue: '£21,500', hint: 'Item cost: £21,500' },
-  alligatorBags: { text: 'Alligator bags', value: 5010, formattedValue: '£5,010', hint: 'Item cost: £5,010' }
+  aboveGroundSteelTank: { text: 'Above-ground steel tank', value: 20250, formattedValue: '£20,250', hint: 'Item cost: £22 per m<sup>3</sup>' },
+  aboveGroundConcreteTank: { text: 'Above-ground concrete tank', value: 20250, formattedValue: '£20,250', hint: 'Item cost: £17 per m<sup>3</sup>' },
+  insituConcreteStore: { text: 'Below-ground in-situ cast-reinforced concrete tank', value: 10000, formattedValue: '£10,000', hint: 'Item cost: £15 per m<sup>3</sup>' },
+  earthBankedLagoon: { text: 'Earth-banked lagoon', value: 9000, formattedValue: '£9,000', hint: 'Item cost: £8 per m<sup>3</sup>' },
+  linedLagoon: { text: 'Earth-bank lagoon (lined)', value: 2100, formattedValue: '£2,100', hint: 'Item cost: £12 per m<sup>3</sup>' },
+  precastConcreteStore: { text: 'Stores using pre-cast rectangular concrete panels', value: 8000, formattedValue: '£8,000', hint: 'Item cost: £14 per m<sup>3</sup>' },
+  alligatorBags: { text: 'Large-volume supported slurry bag', value: 5010, formattedValue: '£5,010', hint: 'Item cost: £20 per m<sup>3</sup>' },
+  slattedFloorStores: { text: 'Slatted-floor stores', value: 21500, formattedValue: '£21,500', hint: 'Item cost: £14 per m<sup>3</sup>' }
+
 }
 
 const coverTypeCost = {
-  fixedCoverLid: { text: 'Fixed cover or lid', value: 1000, formattedValue: '£1,000', hint: 'Item cost: £1,000' },
-  fixedFloatingPlastic: { text: 'Fixed-floating plastic membrane', value: 2100, formattedValue: '£2,100', hint: 'Item cost: £2,100' },
-  freeFloatingPlastic: { text: 'Reception tanks/pits', value: 3250, formattedValue: '£3,250', hint: 'Item cost: £3,250' }
+  rigidCoversConcreteStores: { text: 'Rigid covers for above-ground steel or concrete stores', value: 1000, formattedValue: '£1,000', hint: 'Item cost: £8 per m<sup>2</sup>' },
+  fixedFlexibleCovers: { text: 'Fixed flexible covers for above-ground steel and concrete stores and earth-bank lagoons', value: 2100, formattedValue: '£2,100', hint: 'Item cost: £4 per m<sup>2</sup>' },
+  freeFloatingPlastic: { text: 'Free-floating plastic cover', value: 3250, formattedValue: '£3,250', hint: 'Item cost: £3 per m<sup>2</sup>' }
 }
 
 const projectItemsCost = {
