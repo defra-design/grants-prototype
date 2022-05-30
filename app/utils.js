0a59ba14dd80ad9d6fde9225e1a0f3965f1869ce
Robert Nagy <robert.a.nagy@capgemini.com>
2022-05-30 13:45:49 +0100

    v2 created

--- app/utils.js
+++ app/utils.js
@@ -11,7 +11,8 @@ const serviceName = {
   },
 
 slurrystorage: {
-    current: 'Check if you can apply for a Farming Transformation Fund Slurry Infrastructure Grant'
+    current: 'Check if you can apply for a Farming Transformation Fund Slurry Infrastructure Grant',
+        v1: 'Check if you can apply for a Farming Transformation Fund Slurry Infrastructure Grant'
 },
   slurry: {
     current: 'Check if you can apply for a Farming Transformation Fund Improving Farm Productivity Grant',
