b260e44afe0afa2bfcc85f93134dfdd00eb8471b
Robert Nagy <robert.a.nagy@capgemini.com>
2022-10-31 06:06:03 +0000

    Upgrade kit to v12.3.0

--- docs/documentation_routes.js
+++ docs/documentation_routes.js
@@ -4,8 +4,10 @@ const path = require('path')
 
 // NPM dependencies
 const express = require('express')
-const marked = require('marked')
 const router = express.Router()
+
+// Local dependencies
+const utils = require('../lib/utils')
 
 // Page routes
 
@@ -27,8 +29,8 @@ router.get('/install/:page', function (req, res) {
   }
   redirectMarkdown(req.params.page, res)
   var doc = fs.readFileSync(path.join(__dirname, '/documentation/install/', req.params.page + '.md'), 'utf8')
-  var html = marked(doc)
-  res.render('install_template', { document: html })
+  const renderOptions = utils.getRenderOptions(doc)
+  res.render('install_template', renderOptions)
 })
 
 // When in 'promo mode', redirect to download the current release zip from
@@ -42,7 +44,7 @@ router.get('/download', function (req, res) {
     const version = require('../package.json').version
 
     res.redirect(
-      `https://github.com/alphagov/govuk-prototype-kit/archive/v${version}.zip`
+      `https://github.com/alphagov/govuk-prototype-kit/releases/v${version}/download/govuk-prototype-kit-${version}.zip`
     )
   } else {
     res.redirect(
