761110c8281266fa52e2e8acd87f502be87e8aa9
David Todd <me@david.to>
2024-01-23 14:59:39 +0000

    updates to content

--- app/utils.js
+++ app/utils.js
@@ -1,61 +1,68 @@
 const serviceName = {
-  water: {
-    v2: 'Apply for a large countryside productivity grant for water',
-    v3: 'Apply for a large grant for a water resource management project',
-    v4: 'Apply for a large grant for a water resource management project',
-    v4b: 'Apply for a large grant for a water resource management project',
-    v5: 'Apply for a Farming Transformation Fund water resource management grant',
-    v6: 'Check if you can apply for a water resource management grant',
-    v7private: 'Check if you can apply for a water resource management grant',
-    v8public: 'Check if you can apply for a Farming Transformation Fund water management grant',
-    r2v2: 'Check if you can apply for a Farming Transformation Fund water management grant',
-    current: 'Check if you can apply for a Water Management Grant'
-  },
+	water: {
+		v2: 'Apply for a large countryside productivity grant for water',
+		v3: 'Apply for a large grant for a water resource management project',
+		v4: 'Apply for a large grant for a water resource management project',
+		v4b: 'Apply for a large grant for a water resource management project',
+		v5: 'Apply for a Farming Transformation Fund water resource management grant',
+		v6: 'Check if you can apply for a water resource management grant',
+		v7private: 'Check if you can apply for a water resource management grant',
+		v8public:
+			'Check if you can apply for a Farming Transformation Fund water management grant',
+		r2v2: 'Check if you can apply for a Farming Transformation Fund water management grant',
+		current: 'Check if you can apply for a Water Management Grant',
+	},
 
-  ahw: {
-    current: 'Check if you can apply for a Calf Housing for Health and Welfare Grant',
-    v1: 'Check if you can apply for an Upgrading Cattle Housing Grant',
-    v2: 'Check if you can apply for an Upgrading Cattle Housing Grant',
-    v3: 'Check if you can apply for an Upgrading Cattle Housing Grant',
-    v4: 'Check if you can apply for an Upgrading Cattle Housing Grant',
-    v5: 'Check if you can apply for an Upgrading Cattle Housing Grant',
-    v51: 'Check if you can apply for an Upgrading Cattle Housing Grant',
-    v52t: 'Check if you can apply for an Upgrading Cattle Housing Grant'
-  },
+	ahw: {
+		current:
+			'Check if you can apply for a Calf Housing for Health and Welfare Grant',
+		v1: 'Check if you can apply for an Upgrading Cattle Housing Grant',
+		v2: 'Check if you can apply for an Upgrading Cattle Housing Grant',
+		v3: 'Check if you can apply for an Upgrading Cattle Housing Grant',
+		v4: 'Check if you can apply for an Upgrading Cattle Housing Grant',
+		v5: 'Check if you can apply for an Upgrading Cattle Housing Grant',
+		v51: 'Check if you can apply for an Upgrading Cattle Housing Grant',
+		v52t: 'Check if you can apply for an Upgrading Cattle Housing Grant',
+	},
 
-slurrystorage: {
-    current: 'Check if you can apply for a Slurry Infrastructure Grant',
-        v1: 'Check if you can apply for a Farming Transformation Fund Slurry Infrastructure Grant',
-        v2: 'Check if you can apply for a Farming Transformation Fund Slurry Infrastructure Grant',
-        v3: 'Check if you can apply for a Farming Transformation Fund Slurry Infrastructure Grant',
-        v4: 'Check if you can apply for a Farming Transformation Fund Slurry Infrastructure Grant',
-        r2v1: 'Check if you can apply for a Farming Transformation Fund Slurry Infrastructure Grant',
-        r2v2: 'Check if you can apply for a Farming Transformation Fund Slurry Infrastructure Grant',
-        r2v21: 'Check if you can apply for a Farming Transformation Fund Slurry Infrastructure Grant'
-},
-  slurry: {
-    current: 'Check if you can apply for a Farming Transformation Fund Improving Farm Productivity Grant',
-         v1: 'Check if you can apply for a Farming Transformation Fund productivity grant',
-        v2: 'Check if you can apply for a Farming Transformation Fund productivity grant'
-  },
-  robotics: {
-    current: 'Check if you can apply for a Farming Transformation Fund Improving Farm Productivity Grant',
-         v1: 'Check if you can apply for a Farming Transformation Fund Improving Farm Productivity Grant',
-         v2: 'Check if you can apply for a Farming Transformation Fund Improving Farm Productivity Grant',
-         v3: 'Check if you can apply for a Farming Transformation Fund Improving Farm Productivity Grant',
-         v4: 'Check if you can apply for a Farming Transformation Fund Improving Farm Productivity Grant',
-         v5: 'Check if you can apply for a Farming Transformation Fund Improving Farm Productivity Grant',
-         v6: 'Check if you can apply for a Farming Transformation Fund Improving Farm Productivity Grant',
-         r2v1: 'Check if you can apply for a Farming Transformation Fund Improving Farm Productivity Grant'
-  },
-  layinghens: {
-    current: 'Check if you can apply for a Laying Hens Grant'
-  },
-  addingvalue: {
-    current: 'Check if you can apply for a Farming Transformation Fund Adding Value grant',
-         v1: 'Check if you can apply for a Farming Transformation Fund Adding Value grant',
-         v2: 'Check if you can apply for a Farming Transformation Fund Adding Value grant'
-  }
+	slurrystorage: {
+		current: 'Check if you can apply for a Slurry Infrastructure Grant',
+		v1: 'Check if you can apply for a Farming Transformation Fund Slurry Infrastructure Grant',
+		v2: 'Check if you can apply for a Farming Transformation Fund Slurry Infrastructure Grant',
+		v3: 'Check if you can apply for a Farming Transformation Fund Slurry Infrastructure Grant',
+		v4: 'Check if you can apply for a Farming Transformation Fund Slurry Infrastructure Grant',
+		r2v1: 'Check if you can apply for a Farming Transformation Fund Slurry Infrastructure Grant',
+		r2v2: 'Check if you can apply for a Farming Transformation Fund Slurry Infrastructure Grant',
+		r2v21:
+			'Check if you can apply for a Farming Transformation Fund Slurry Infrastructure Grant',
+	},
+	slurry: {
+		current:
+			'Check if you can apply for a Farming Transformation Fund Improving Farm Productivity Grant',
+		v1: 'Check if you can apply for a Farming Transformation Fund productivity grant',
+		v2: 'Check if you can apply for a Farming Transformation Fund productivity grant',
+	},
+	robotics: {
+		current:
+			'Check if you can apply for a Farming Transformation Fund Improving Farm Productivity Grant',
+		v1: 'Check if you can apply for a Farming Transformation Fund Improving Farm Productivity Grant',
+		v2: 'Check if you can apply for a Farming Transformation Fund Improving Farm Productivity Grant',
+		v3: 'Check if you can apply for a Farming Transformation Fund Improving Farm Productivity Grant',
+		v4: 'Check if you can apply for a Farming Transformation Fund Improving Farm Productivity Grant',
+		v5: 'Check if you can apply for a Farming Transformation Fund Improving Farm Productivity Grant',
+		v6: 'Check if you can apply for a Farming Transformation Fund Improving Farm Productivity Grant',
+		r2v1: 'Check if you can apply for a Farming Transformation Fund Improving Farm Productivity Grant',
+	},
+	layinghens: {
+		current:
+			'Check if you can apply for a Laying Hen Housing for Health and Welfare Grant',
+	},
+	addingvalue: {
+		current:
+			'Check if you can apply for a Farming Transformation Fund Adding Value grant',
+		v1: 'Check if you can apply for a Farming Transformation Fund Adding Value grant',
+		v2: 'Check if you can apply for a Farming Transformation Fund Adding Value grant',
+	},
 }
 
 const schemeList = Object.keys(serviceName)
